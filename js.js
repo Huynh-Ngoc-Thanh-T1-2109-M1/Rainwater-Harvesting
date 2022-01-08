842329cd5576d5390d98a75e104414395db00248
Huynh-Ngoc-Thanh-T1-2109-M1 <thanhhnts2109011@fpt.edu.vn>
2022-01-08 20:13:51 +0700

    update

--- js.js
+++ js.js
@@ -4,22 +4,27 @@ var app = angular.module("myApp", ["ngRoute"]);
 // Get json file function
 app.run(function ($rootScope, $http) {
 
-    $http.get("accessories.json").then(function (response) {
+    $http.get("accessories.json").then(function (response){
         $rootScope.accessories = response.data.accessories;
         console.log($rootScope.accessories);
     });
 
-    $http.get("pumpControl.json").then(function (response) {
+    $http.get("pumpControl.json").then(function (response){
         $rootScope.pumpControl = response.data.pumpControl;
         console.log($rootScope.pumpControl);
     });
 
-    $http.get("water_sterilisation.json").then(function (response) {
+    $http.get("filters.json").then(function (response){
+      $rootScope.filters = response.data.filters;
+      console.log($rootScope.filters);
+    });
+
+    $http.get("water_sterilisation.json").then(function (response){
         $rootScope.water_sterilisation = response.data.water_sterilisation;
         console.log($rootScope.water_sterilisation);
     });
 
-    $http.get("tanks.json").then(function (response) {
+    $http.get("tanks.json").then(function (response){
         $rootScope.tanks = response.data.tanks;
         console.log($rootScope.tanks);
     });
